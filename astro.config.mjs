bea74b427460122f833774095e52513c6d6225ca
a-chacon <andres.ch@protonmail.com>
2024-12-20 09:24:25 -0300

    feat: add github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,9 @@ import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
-vite: {
+  vite: {
     plugins: [tailwindcss()],
   },
-    // add yur domain name here
-   site: 'https://lexingtonthemes.com',
+  site: 'https://chaski.a-chacon.com',
   integrations: [sitemap()]
 });
