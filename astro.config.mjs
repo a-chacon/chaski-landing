e57cbd2affc9e8aa3608f85c78fa5e4ee931e062
achacon <andres.ch@protonmail.com>
2025-03-06 13:18:46 +0100

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: 'https://a-chacon.com',
+  site: 'https://chaski.a-chacon.com',
   integrations: [sitemap()]
 });
